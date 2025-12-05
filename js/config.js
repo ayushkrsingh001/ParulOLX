54435f947c7fc3c7e574c133325bffcd6aa73ec0
ayushkumarsingh001 <ayushkrsingh91131@gmail.com>
2025-12-05 14:02:32 +0530

    Fix2

---
+++ js/config.js
@@ -0,0 +1,20 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyBdEKHYMblxuxfYPBvACAMEHnSR-m2fF-U",
+        authDomain: "parulolx.firebaseapp.com",
+        projectId: "parulolx",
+        storageBucket: "parulolx.firebasestorage.app",
+        messagingSenderId: "392039663484",
+        appId: "1:392039663484:web:15700a2a18cf42e3f986f9",
+        measurementId: "G-SKKWCX5X9J"
+    },
+    emailJs: {
+        publicKey: "gGF6eyo9Jw71GC7bY"
+    },
+    cloudinary: {
+        cloudName: "dnibuk426",
+        uploadPreset: "ParulOLX"
+    }
+};
+
+export default config;
